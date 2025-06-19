c8867d8928f051c419e11d2beb345356d367abe0
william77910 <williamfaucomprez@gmail.com>
2025-06-19 20:56:12 +0200

    Ajout de la gestion des événements pour le formulaire et affichage du texte saisi en temps réel

--- Asset/js/dom.js
+++ Asset/js/dom.js
@@ -68,7 +68,7 @@ editerMoi.appendChild(nouveauParagraphe);
 	box.classList.toggle('rond');
 })
 */
-
+//-------------------------------------------------------------------------------------------
 //Création et suppression d'éléments
 // document.createElement('div);   Créer un nouvel élément
 // parent.appendChild(element);  Ajoutne un élément enfant
@@ -80,7 +80,7 @@ editerMoi.appendChild(nouveauParagraphe);
 // 2 Ajouter un bouton de suppression à chauqe élément
 // 3 Implémenter la suppresssion d'éléments
 
-const boutonAjout = document.getElementById('ajout');
+/*const boutonAjout = document.getElementById('ajout');
 const liste = document.getElementById('items');
 let compteur = 1;
 
@@ -105,6 +105,7 @@ function ajouterElement(){
 	boutonSupprimer.onclick = function(){
 		//implémenter la suppression
 		li.remove();  // Alternative : liste.removeChild(li);
+		compteur--;   // Décrémenter le compteur
 	};
 
 	//Assembler et ajouter à la liste
@@ -112,7 +113,84 @@ function ajouterElement(){
 	li.appendChild(boutonSupprimer);
 	liste.appendChild(li);
 
-	compteur++;
+	compteur++;  // Incrémenter le compteur pour le prochain élément
 }
 //Attacher l'événement au bouton
 boutonAjout.addEventListener('click', ajouterElement);
+*/
+//-----------------------------------------------------------------------------------
+//Gestion des événements
+//click
+//submit
+//keyup / keydown
+//mouseover / mouseout
+//change
+
+//Exercice
+//1 Empêcher le comportement par défaut du formulaire
+//2 Afficher le texte saisi en temps réel
+//3 Valider le contenu avant soumission
+
+//Pseudo code
+//Sélectionner les éléments necessaires
+//formulaire = sélectionner élément avec ID "monFormulaire"
+//champNom = sélectionner élément avec ID "monInput"
+//divResultat = sélectionner élément avec ID "resultat"
+
+//1 Empêcher le comportement par défaut du formulaire
+//Ajouter un écouteur d'évenement "submit" sur le formulaire
+//Empêcher le comportement par défault
+
+//3 valider le contenu avant soumission
+	/* si champNom est vide alors
+		Afficher message d'erreur dans la div resultat
+		mettre divResultat en rouge
+		terminer la fonction
+	sinon 
+		Afficher message de succés dans la divResultat
+		mettre la divResultat en vert
+		réinitialise le formulaire
+	fin si
+	*/
+
+//2 Afficher le texte saisi en temps réel
+	/* Ajouter un ecouteur d'evenement sur "input" champNom
+
+		si la valeur saisie est vide alors
+			Afficher message d'attente sans la divResultat
+			Mettre la divResultat en gris
+		sinon
+			Afficher texte saisi dans div
+			Mettre divResultat en bleu
+		fin si
+	*/
+
+const form = document.getementById('monFormulaire');
+const monInput = document.getElementById('nomInput');
+const resultat = document.getElementById('resultat');
+
+//1 Empêcher le comportement par defaut du formulaire
+form.addEventListener('submit', function(e){
+	e.preventDefault();
+
+	//3 valider le contenu avant soumission
+	if(monInput.Value.trim() === ''){
+		resultat.textContent = 'Erreur : Le champ ne peut pas être vide';
+		resultat.style.color = 'red';
+		return;
+	}
+	resultat.textContent = 'Formulaire soumis avec le nom : ${nomInput.value}';
+	resultat.style.color = 'green';
+	form.reset();
+})
+
+//2 Afficher le texte saisi en temps réel
+nomInput.addEventListener('input', function(){
+	if(this.value.trim() === '') {
+		resultat.textContent = 'En attente de saisi....';
+		resultat.style.color = 'gray';
+	}else{
+		resultat.textContent = 'Vous tapez : ${this.value}';
+		resultat.style.color = 'blue';
+	}
+})
